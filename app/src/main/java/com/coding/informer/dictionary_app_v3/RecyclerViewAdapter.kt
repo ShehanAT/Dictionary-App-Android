b5cb12e6bd25764d9bd53c259c439915f608f073
Nipun <shehanatuk@gmail.com>
2023-06-08 16:11:24 -0400

    Added RecyclerViewAdapter and RecycleViewHolders files

---
+++ app/src/main/java/com/coding/informer/dictionary_app_v3/RecyclerViewAdapter.kt
@@ -0,0 +1,22 @@
+package com.coding.informer.dictionary_app_v3
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+internal class RecyclerViewAdapter(context: Context?, private val itemList: List<WordObject>) :
+    RecyclerView.Adapter<RecyclerViewHolders>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolders {
+        val layoutView = LayoutInflater.from(parent.context).inflate(R.layout.list_layout, null)
+        return RecyclerViewHolders(layoutView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolders, position: Int) {
+        holder.awardTitle.setText("Word: " + itemList[position].word)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+}
--- app/src/main/java/com/coding/informer/dictionary_app_v3/WordObject.kt
+++ app/src/main/java/com/coding/informer/dictionary_app_v3/WordObject.kt
@@ -1,3 +1,8 @@
 package com.coding.informer.dictionary_app_v3
 
-internal class WordObject(val word: String)
+internal class WordObject {
+
+    var word: String = ""
+        get() = field
+        set(value) { field = value }
+}
